857adda005ef1af38e169b431851c31296c593a7
Pavel Pavlov <resolversoft@gmail.com>
2021-04-13 14:50:28 +0300

    added server path

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 require('dotenv').config()
 const app = express();
-
+const path = require("path")
 
 const articlesRouter = require('./routes/articles');
 const db_conn = process.env.MONGO_URI;
@@ -15,6 +15,12 @@ mongoose.connect(db_conn,{useNewUrlParser:true,useUnifiedTopology:true})
 app.use(cors());
 app.use(express.json());
 app.use('/articles',articlesRouter);
+
+app.use(express.static(path.join(__dirname, "client", "build")))
+app.use(express.static(path.join(__dirname,'build')));
+app.get('/*',(req,res) =>{
+res.sendFile(path.join(__dirname,'build','index.html'));
+
 
 
 const port = process.env.PORT || 8080;
