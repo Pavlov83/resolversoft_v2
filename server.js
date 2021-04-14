0d5fd4719c3768582bae47dce6d15ed905124e9a
p <p@ps-MacBook-Pro.local>
2021-04-14 21:40:58 +0300

    added build

--- server.js
+++ server.js
@@ -16,11 +16,12 @@ app.use(cors());
 app.use(express.json());
 app.use('/articles',articlesRouter);
 
-app.use(express.static(path.join(__dirname, "client", "build")))
+app.use(express.static(path.join(__dirname, 'client', 'build')));
 app.use(express.static(path.join(__dirname,'build')));
 app.get('/*',(req,res) =>{
 res.sendFile(path.join(__dirname,'build','index.html'));
 
+});
 
 
 const port = process.env.PORT || 8080;
